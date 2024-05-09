0e4868058acdfb8ed57fb6b5310966c0cb6df2be
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2024-05-09 13:01:36 +0530

    Update update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -19,37 +19,55 @@
 #       chmod +x $USER_SCRIPTS/update_all
 #  and now you can call the script any time :)
 
+# Check if script is run with sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script with sudo."
+    exit 1
+fi
 
 # Text Color Variables
-readonly RED='\033[31m'   # Red
-readonly GREEN='\033[32m' # Green
-readonly CLEAR='\033[0m'  # Clear color and formatting
+readonly RED='\033[31m'      # Red
+readonly GREEN='\033[32m'    # Green
+readonly CLEAR='\033[0m'     # Clear color and formatting
 
 # Function to update Ubuntu
 update_ubuntu() {
-    echo "Updating Ubuntu..."
-    apt update -y
-    apt upgrade -y
-    apt autoremove -y
-    echo "Update completed."
+    echo -e "${GREEN}Updating Ubuntu...${CLEAR}"
+    apt update -y && apt upgrade -y && apt autoremove -y
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}Update completed.${CLEAR}"
+    else
+        echo -e "${RED}Update failed.${CLEAR}"
+        exit 1
+    fi
 }
 
-# Function to update CentOS
+# Function to update CentOS or Fedora
 update_centos() {
-    echo "Updating CentOS..."
-    yum update -y
-    yum upgrade -y
-    yum autoremove -y
-    echo "Update completed."
+    echo -e "${GREEN}Updating CentOS...${CLEAR}"
+    if command -v dnf &>/dev/null; then
+        dnf upgrade -y && dnf autoremove -y
+    else
+        yum update -y && yum autoremove -y
+    fi
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}Update completed.${CLEAR}"
+    else
+        echo -e "${RED}Update failed.${CLEAR}"
+        exit 1
+    fi
 }
 
 # Function to update Debian
 update_debian() {
-    echo "Updating Debian..."
-    apt-get update -y
-    apt-get upgrade -y
-    apt-get autoremove -y
-    echo "Update completed."
+    echo -e "${GREEN}Updating Debian...${CLEAR}"
+    apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}Update completed.${CLEAR}"
+    else
+        echo -e "${RED}Update failed.${CLEAR}"
+        exit 1
+    fi
 }
 
 # Check if the OS is Ubuntu
@@ -61,8 +79,8 @@ if [ -f /etc/lsb-release ]; then
     fi
 fi
 
-# Check if the OS is CentOS
-if [ -f /etc/centos-release ]; then
+# Check if the OS is CentOS or Fedora
+if [ -f /etc/redhat-release ] || [ -f /etc/centos-release ]; then
     update_centos
     exit 0
 fi
@@ -72,3 +90,7 @@ if [ -f /etc/debian_version ]; then
     update_debian
     exit 0
 fi
+
+echo "Unsupported Linux distribution."
+exit 1
+
