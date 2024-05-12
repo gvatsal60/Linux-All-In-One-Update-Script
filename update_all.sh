2941ba0fea63e60e70030da5ba86c67a6d385e81
Vatsal Gupta <gvatsal60@gmail.com>
2024-05-12 14:25:40 +0530

    Updated with more syntax check

--- update_all.sh
+++ update_all.sh
@@ -1,111 +1,79 @@
 # shellcheck shell=sh
 
 # Text Color Variables
-readonly RED='\033[31m'   # Red
-readonly GREEN='\033[32m' # Green
-readonly CLEAR='\033[0m'  # Clear color and formatting
+RED=$(tput setaf 1)   # Red
+GREEN=$(tput setaf 2) # Green
+CLEAR=$(tput sgr0)    # Clear color and formatting
 
-# Global Variables
-SUDO_USER=$(logname)
-
-# Function to check if script is run with sudo
-check_sudo() {
-    if [ "$(id -u)" -ne 0 ]; then
-        printf "\n%sPlease run this script with sudo!!!%s" "${RED}" "${CLEAR}"
-        return 255
-    fi
-}
-
-# OS Update Function
+# OS Update Functions
 
 # Function to update Debian based
 update_debian() {
-    printf "\n%sUpdating Debian based...%s" "${GREEN}" "${CLEAR}"
-    if apt-get update -y && apt-get upgrade -y && apt-get autoremove -y; then
-        printf "\n%s" "${GREEN}Update completed.${CLEAR}"
-    else
-        printf "\n%s" "${RED}Update failed.${CLEAR}"
-        return
+    printf "\n%sUpdating Debian based...\n%s" "${GREEN}" "${CLEAR}"
+    if ! sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y; then
+        printf "\n%sUpdate failed.\n%s" "${RED}" "${CLEAR}"
     fi
 }
 
 # Function to update RPM based
 update_rpm() {
-    printf "\n%s" "${GREEN}Updating RPM based...${CLEAR}"
+    printf "\n%sUpdating RPM based...\n%s" "${GREEN}" "${CLEAR}"
     if command -v dnf >/dev/null 2>&1; then
-        if dnf upgrade -y && dnf autoremove -y; then
-            printf "\n%s" "${GREEN}Update completed.${CLEAR}"
-        else
-            printf "\n%s" "${RED}Update failed.${CLEAR}"
-            return
+        if ! sudo dnf upgrade -y && sudo dnf autoremove -y; then
+            printf "\n%sUpdate failed.\n%s" "${RED}" "${CLEAR}"
+        fi
+    elif command -v yum >/dev/null 2>&1; then
+        if ! sudo yum update -y && sudo yum autoremove -y; then
+            printf "\n%sUpdate failed.\n%s" "${RED}" "${CLEAR}"
         fi
     else
-        if yum update -y && yum autoremove -y; then
-            printf "\n%s" "${GREEN}Update completed.${CLEAR}"
-        else
-            printf "\n%s" "${RED}Update failed.${CLEAR}"
-            return
-        fi
+        printf "\n%sUpdate failed unknown package manager\n%s" "${RED}" "${CLEAR}"
     fi
 }
 
 # Function to update Pacman based
 update_pacman() {
-    printf "\n%s" "${GREEN}Updating Pacman based...${CLEAR}"
-
-    # Check if pacman is available
-    if ! command -v pacman >/dev/null 2>&1; then
-        printf "\n%s" "${RED}Pacman is not installed.${CLEAR}"
-        return 1
-    fi
-
-    # Update pacman packages and check if the update was successful
-    if pacman -Syu --noconfirm; then
-        printf "\n%s" "${GREEN}Update completed.${CLEAR}"
-    else
-        printf "\n%s" "${RED}Update failed.${CLEAR}"
-        return
+    printf "\n%sUpdating Pacman based...\n%s" "${GREEN}" "${CLEAR}"
+    if command -v pacman >/dev/null 2>&1; then
+        if ! sudo pacman -Syu --noconfirm; then
+            printf "\n%sUpdate failed.\n%s" "${RED}" "${CLEAR}"
+        fi
     fi
 }
 
 # Function to update os based on their type
 update_os() {
-    printf "\n%s" "${GREEN}Update OS Packages${CLEAR}"
+    printf "\n%sUpdate OS Packages\n%s" "${GREEN}" "${CLEAR}"
 
-    # Check if script is run with sudo
-    if check_sudo; then
-        # Check if the OS is Debian
-        if [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
-            update_debian
-        # Check if the OS is RPM based
-        elif [ -f /etc/redhat-release ] || [ -f /etc/centos-release ]; then
-            update_rpm
-        # Check if the OS is Pacman based
-        elif [ -f /etc/arch-release ]; then
-            update_pacman
-        else
-            echo "Unsupported Linux distribution."
-        fi
+    # Check if the OS is Debian
+    if [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
+        update_debian
+    # Check if the OS is RPM based
+    elif [ -f /etc/redhat-release ] || [ -f /etc/centos-release ]; then
+        update_rpm
+    # Check if the OS is Pacman based
+    elif [ -f /etc/arch-release ]; then
+        update_pacman
+    else
+        printf "\n%sUnsupported Linux distribution.%s\n" "${RED}" "${CLEAR}"
     fi
 }
 
 # Function to update vscode extensions
 update_vscode_ext() {
-    printf "\n\n%sUpdating VSCode Extensions%s" "${GREEN}" "${CLEAR}"
+    printf "\n%sUpdating VSCode Extensions%s\n" "${GREEN}" "${CLEAR}"
 
     if ! command -v code >/dev/null 2>&1; then
         printf "\n%sVSCode is not installed.%s" "${RED}" "${CLEAR}"
         return
     fi
 
-    CODE_DIR=$(dirname "$(command -v code)")
-
-    sudo -u "$SUDO_USER" code --update-extensions --no-sandbox --user-data-dir="$CODE_DIR"
+    code --update-extensions --no-sandbox
 }
 
 # Function to update gem packages
 update_gem() {
-    printf "\n\n%s" "${GREEN}Updating Gems${CLEAR}"
+    printf "\n%sUpdating Gems%s" "${GREEN}" "${CLEAR}"
 
     if ! command -v gem >/dev/null 2>&1; then
         printf "\n%s" "${RED}Gem is not installed.${CLEAR}"
@@ -117,7 +85,7 @@ update_gem() {
 
 # Function to update npm packages
 update_npm() {
-    printf "\n\n%s" "${GREEN}Updating Npm Packages${CLEAR}"
+    printf "\n%sUpdating Npm Packages%s" "${GREEN}" "${CLEAR}"
 
     if ! command -v npm >/dev/null 2>&1; then
         printf "\n%s" "${RED}Npm is not installed.${CLEAR}"
@@ -129,7 +97,7 @@ update_npm() {
 
 # Function to update yarn packages
 update_yarn() {
-    printf "\n\n%s" "${GREEN}Updating Yarn Packages${CLEAR}"
+    printf "\n%sUpdating Yarn Packages%s" "${GREEN}" "${CLEAR}"
 
     if ! command -v yarn >/dev/null 2>&1; then
         printf "\n%s" "${RED}Yarn is not installed.${CLEAR}"
@@ -141,7 +109,7 @@ update_yarn() {
 
 # Function to update pip3 packages
 update_pip3() {
-    printf "\n\n%s" "${GREEN}Updating Python 3.x pips${CLEAR}"
+    printf "\n%sUpdating Python 3.x pips%s" "${GREEN}" "${CLEAR}"
 
     if ! command -v python3 >/dev/null 2>&1 || ! command -v pip3 >/dev/null 2>&1; then
         printf "\n%sPython 3 or pip3 is not installed.%s" "${RED}" "${CLEAR}"
@@ -149,7 +117,7 @@ update_pip3() {
     fi
 
     # Running with a non-root user
-    sudo -u "$SUDO_USER" pip3 list --outdated --format=columns | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
+    python3 -m pip list --outdated --format=columns | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 python3 -m pip install -U
 }
 
 # Function to update all in one shot
@@ -162,8 +130,9 @@ update_all() {
         update_npm
         update_yarn
         update_pip3
+        printf "\n"
     else
-        printf "\n%sInternet Disabled!!!%s" "${RED}" "${CLEAR}"
+        printf "\n%sInternet Disabled!!!%s\n" "${RED}" "${CLEAR}"
     fi
 }
 
