92803ed98b5584f1237be73b8f6498a1bba965ac
VG <gvatsal@gmail.com>
2024-07-11 22:00:14 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -16,36 +16,54 @@
 ##########################################################################################
 # Constants
 ##########################################################################################
+readonly FILE_NAME=".update.sh"
+readonly FILE_LINK="https://raw.githubusercontent.com/gvatsal60/Linux-All-In-One-Update-Script/HEAD/${FILE_NAME}"
+
 UPDATE_RC="${UPDATE_RC:-"true"}"
 
 ##########################################################################################
 # Functions
 ##########################################################################################
 
-# Function to update shell configuration files
+# Function: updaterc
+# Description: Update shell configuration files
 updaterc() {
     _rc=""
     if [ "${UPDATE_RC}" = "true" ]; then
         case $ADJUSTED_ID in
-            debian | rhel)
-                echo "Updating ~/.bashrc for ${ADJUSTED_ID}..."
-                _rc=~/.bashrc
-                ;;
-            alpine | arch)
-                echo "Updating ~/.profile for ${ADJUSTED_ID}..."
-                _rc=~/.profile
-                ;;
-            *)
-                echo "Updating shell configuration files for ${ADJUSTED_ID}..."
-                echo "Unsupported or unrecognized Linux distribution."
-                exit 1
-                ;;
+        debian | rhel)
+            echo "Updating ~/.bashrc for ${ADJUSTED_ID}..."
+            _rc=~/.bashrc
+            ;;
+        alpine | arch)
+            echo "Updating ~/.profile for ${ADJUSTED_ID}..."
+            _rc=~/.profile
+            ;;
+        *)
+            echo "Error: Unsupported or unrecognized Linux distribution ${ADJUSTED_ID}"
+            exit 1
+            ;;
         esac
 
         # Check if alias update='sudo sh ~/.update.sh' is already defined, if not then append it
         if ! grep -qxF "alias update='sudo sh ~/.update.sh'" "${_rc}"; then
-            printf "\n# Alias for Update\nalias update='sudo sh ~/.update.sh'\n" >> "${_rc}"
+            printf "\n# Alias for Update\nalias update='sudo sh ~/.update.sh'\n" >>"${_rc}"
         fi
+    fi
+}
+
+# Function: dw_file
+# Description: Download alias file using wget or curl if available
+dw_file() {
+    # Check if wget is available
+    if command -v wget >/dev/null 2>&1; then
+        wget -O "${HOME}/${FILE_NAME}" ${FILE_LINK}
+    # Check if curl is available
+    elif command -v curl >/dev/null 2>&1; then
+        curl -fsSL -o "${HOME}/${FILE_NAME}" ${FILE_LINK}
+    else
+        printf "Either install wget or curl"
+        exit 1
     fi
 }
 
@@ -71,4 +89,20 @@ else
     exit 1
 fi
 
-updaterc
+if [ -f "${HOME}/${FILE_NAME}" ]; then
+    printf "File already exists: %s\n" "$HOME/${FILE_NAME}"
+    printf "Do you want to replace it? [y/n]: "
+    read -r rp_conf
+    rp_conf="${rp_conf:-y}"
+    if [ "$rp_conf" = "y" ]; then
+        # Replace the existing file
+        printf "\nReplacing %s...\n" "$HOME/${FILE_NAME}"
+        dw_file
+        updaterc
+    else
+        printf "\nKeeping existing file: %s\n" "$HOME/${FILE_NAME}"
+    fi
+else
+    dw_file
+    updaterc
+fi
